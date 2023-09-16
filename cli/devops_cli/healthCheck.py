9408997016f68c595ce3ace1e13e3d4bdaea1325
sergiustavila <sergiu.stavila@gmail.com>
2023-09-16 20:26:07 +0300

    Updated healthCheck.sh

--- cli/devops_cli/healthCheck.py
+++ cli/devops_cli/healthCheck.py
@@ -10,28 +10,27 @@ def cli():
 @cli.command("date_time", short_help="Show current date and time")
 def date_time():
     current_datetime = datetime.datetime.now()
-    click.echo("Date and Time: {}".format(current_datetime))
+    click.echo(f"Date and Time: {current_datetime}")
 
-@cli.command("get_machine_uptime", short_help="Show machine uptime")
+@cli.command("uptime", short_help="Show machine uptime")
 def get_machine_uptime():
     try:
-        uptime_output = subprocess.check_output(['uptime'], universal_newlines=True)
+        # For Unix-like systems
+        uptime = subprocess.check_output(["uptime -p"], universal_newlines=True).strip()
+    except:
+        #  For Windows
+        uptime = psutil.boot_time()
+        uptime = datetime.datetime.fromtimestamp(uptime).strftime("%Y-%m-%d %H:%M:%S")
 
-        click.echo("Machine Uptime: {}".format(uptime_output.strip()))
-    except subprocess.CalledProcessError:
-         click.echo("Unable to retrieve uptime information.")
+    click.echo(f"Machine Uptime: {uptime}")
 
-@cli.command("get_disk_usage", short_help="Show disk usage")
+@cli.command("disk", short_help="Show disk usage")
 def get_disk_usage():
-    partitions = psutil.disk_partitions()
-    disk_usage_info = []
-
-    for partition in partitions:
-        usage = psutil.disk_usage(partition.mountpoint)
-        disk_usage_info.append(f"{partition.device} - Total: {usage.total / (1024 ** 3):.2f} GB, "
-                               f"Used: {usage.used / (1024 ** 3):.2f} GB, "
-                               f"Free: {usage.free / (1024 ** 3):.2f} GB")
-    click.echo(disk_usage_info)
+    disk_info = psutil.disk_usage("/")
+    click.echo(f"Total Disk Space: {disk_info.total / (1024 ** 3):.2f} GB")
+    click.echo(f"Free Disk Space: {disk_info.free / (1024 ** 3):.2f} GB")
+    click.echo(f"Used Disk Space: {disk_info.used / (1024 ** 3):.2f} GB")
+    click.echo(f"Disk Space Percentage: {disk_info.percent} %")
 
 @cli.command("get_memory_usage", short_help="Show memory usage")
 def get_memory_usage():
